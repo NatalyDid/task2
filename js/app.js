2cd765a5d0d66a08fa5bf0a2e9a847ab67903168
NatalyDid <35141520+NatalyDid@users.noreply.github.com>
2018-02-23 14:59:50 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 // подключаемся к firebase для регистрации
 
 $(function () {
-
+    const errors = $("#errors");
+    const failed = {fail: false, errors: []};
+    const userName = $(".user-name");
+    //var isLoggedIn;
 
     // создаем сессию если пользователь уже логинился
-    //initSession();
-    isAlreadyLogged();
+    initSession();
 
     // обработка формы регистрации
     handleFormRegistration();
@@ -16,174 +18,158 @@ $(function () {
     // выйти
     logOut();
 
-});
-
-
-const errors = $("#errors");
-const failed = {fail: false, errors: []};
-const userName = $(".user-name");
-var isLoggedIn;
-
-function isAlreadyLogged() {
-    if (isLoggedIn) {
-        //$('ul.autorization').toggleClass("display_none");
-        //$('div.welcome').toggleClass("display_none");
-        console.log('gggggg');
-    }
-}
-
-// удаленике ошибок после редактировании формы
-function changeForm(form) {
-    form.change(function () {
-        errors.text("");
-        failed.fail = false;
-        failed.errors = [];
-    });
-}
-
-// инициализация сессии
-function initSession() {
-
-    firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
-            userName.text("Welcome, " + user.displayName + "!");
-            //$('ul.autorization').toggleClass("display_none");
-            //$('div.welcome').toggleClass("display_none");
-            /*$('div.controls').css("display", "block");
-            $('.sidebar #burger').css("display", "block");*/
-            isLoggedIn = true;
-            console.log('lalala');
-
+    /*function isAlreadyLogged() {
+        if (!isLoggedIn){
+            $('ul.autorization').toggleClass("display_none");
+            $('div.welcome').toggleClass("display_none");
         }
+    }*/
 
-    });
-}
-
-function createUser(email, password, name) {
-    firebase
-        .auth()
-        .createUserWithEmailAndPassword(email, password)
-        .then(function (user) {
-            console.log(user);
-            user
-                .updateProfile({
-                    displayName: name
-                })
-                .then(function () {
-                    initSession()
-                });
-        })
-        .catch(function (err) {
-            // обрабатываем ошибки
-            // выведу над формой
-            errors.text(err.message);
+    // удаленике ошибок после редактировании формы
+    function changeForm(form) {
+        form.change(function () {
+            errors.text("");
+            failed.fail = false;
+            failed.errors = [];
         });
-}
+    }
 
-function handleFormRegistration() {
-    const form = $('form[name=register]');
+    // инициализация сессии
+    function initSession() {
+        //isLoggedIn = true;
+        firebase.auth().onAuthStateChanged(function (user) {
+            if (user) {
+                userName.text("Welcome, " + user.displayName + "!");
+                $('ul.autorization').css("display", "none");
+                $('div.welcome').css("display", "block");
+                $('div.controls').removeClass("display_none");
+                $('.sidebar #burger').css("display", "block");
 
-    changeForm(form);
-
-    $('#register').click(function (event) {
-        event.preventDefault();
-
-        //const {elements} = event.target;
-
-        const name = $('#register_login').val(); // Имя пользователя
-        const email = $('#register_email').val(); // email
-        const password = $('#register_password').val(); // password
-        const repeatPassword = $('#register_confirmation').val(); // repeat password
-
-        // делаем валидацию формы, савниваем пароли если нужно ...
-
-        // примитивная валидация
-        if (password !== repeatPassword) {
-            failed.fail = true;
-            failed.errors.push("Пароли не совпадают!");
-        }
-
-        if (name.length === 0) {
-            failed.fail = true;
-            failed.errors.push("Введите имя");
-        }
-
-        // подключаемся к firebase если пароли совпали и поле имя не пустое
-        if (!failed.fail) {
-            // сохраняем юзера
-            createUser(email, password, name);
-
-            $('#register_login').value = "";
-            $('#register_email').value = "";
-            $('#register_password').value = "";
-            $('#register_confirmation').value = "";
-        } else {
-            // если есть ошибки, выводим на экран
-            var stringErrors = "";
-            for (var error in failed.errors) {
-                stringErrors += error + "<br>";
+                //console.log('lalala');
             }
-            errors.text(stringErrors);
-        }
-    });
-}
 
-// форма аутентификации
-function handleFormLogin() {
-    const form = $('form[name=auth]');
-    changeForm(form);
-    // делаешь какую нибудь проверку пароля и почты, потом авторизируешь
-    $('#auth').click(function (event) {
-        event.preventDefault();
-        isLoggedIn = true;
-        const email = $('#auth_login').val(); // первый input в форме
-        const password = $('#auth_password').val(); // второй input в форме
+        });
+    }
 
+    function createUser(email, password, name) {
         firebase
             .auth()
-            .signInWithEmailAndPassword(email, password)
+            .createUserWithEmailAndPassword(email, password)
             .then(function (user) {
-                // наши действия
-                initSession();
-                $('#auth_login').value = '';
-                $('#auth_password').value = '';
+                console.log(user);
+                user
+                    .updateProfile({
+                        displayName: name
+                    })
+                    .then(function () {
+                        initSession()
+                    });
             })
             .catch(function (err) {
-                // обработка ошибок
+                // обрабатываем ошибки
+                // выведу над формой
                 errors.text(err.message);
-                console.log(err);
             });
-    });
-}
+    }
 
-// выход
-function logOut() {
+    function handleFormRegistration() {
+        const form = $('form[name=register]');
 
-    const btnLogout = $('#btnLogout');
-    btnLogout.click(function () {
-        isLoggedIn = false;
+        changeForm(form);
 
-        firebase
-            .auth()
-            .signOut().then(function () {
-            //$('div.welcome').toggleClass("display_none");
-            //$('ul.autorization').toggleClass("display_none");
-            //$('div.controls').toggleClass("display_none");
-            //$('.sidebar #burger').toggleClass("display_none");
-            userName.text("Welcome");
+        $('#register').click(function (event) {
+            event.preventDefault();
 
-        })
-            .catch(function (err) {
-                console.log(err);
-            });
-    });
-}
+            //const {elements} = event.target;
 
+            const name = $('#register_login').val(); // Имя пользователя
+            const email = $('#register_email').val(); // email
+            const password = $('#register_password').val(); // password
+            const repeatPassword = $('#register_confirmation').val(); // repeat password
 
-$(function () {
+            // делаем валидацию формы, савниваем пароли если нужно ...
+
+            // примитивная валидация
+            if (password !== repeatPassword) {
+                failed.fail = true;
+                failed.errors.push("Пароли не совпадают!");
+            }
+
+            if (name.length === 0) {
+                failed.fail = true;
+                failed.errors.push("Введите имя");
+            }
+
+            // подключаемся к firebase если пароли совпали и поле имя не пустое
+            if (!failed.fail) {
+                // сохраняем юзера
+                createUser(email, password, name);
+
+                $('#register_login').value = "";
+                $('#register_email').value = "";
+                $('#register_password').value = "";
+                $('#register_confirmation').value = "";
+            } else {
+                // если есть ошибки, выводим на экран
+                let stringErrors = "";
+                for (let error of failed.errors) {
+                    stringErrors += error + "<br>";
+                }
+                errors.text(stringErrors);
+            }
+        });
+    }
+
+    // форма аутентификации
+    function handleFormLogin() {
+        const form = $('form[name=auth]');
+        changeForm(form);
+        // делаешь какую нибудь проверку пароля и почты, потом авторизируешь
+        $('#auth').click(function (event) {
+            event.preventDefault();
+            const email = $('#auth_login').val(); // первый input в форме
+            const password = $('#auth_password').val(); // второй input в форме
+
+            firebase
+                .auth()
+                .signInWithEmailAndPassword(email, password)
+                .then(function (user) {
+                    // наши действия
+                    initSession();
+                    $('#auth_login').value = '';
+                    $('#auth_password').value = '';
+                })
+                .catch(function (err) {
+                    // обработка ошибок
+                    errors.text(err.message);
+                    console.log(err);
+                });
+        });
+    }
+
+    // выход
+    function logOut() {
+        //isLoggedIn = false;
+        const btnLogout = $('#btnLogout');
+        btnLogout.click(function () {
+            firebase
+                .auth()
+                .signOut().then(function () {
+                $('div.welcome').css("display", "none");
+                $('ul.autorization').css("display", "block");
+                $('div.controls').css("display", "none");
+                $('.sidebar #burger').css("display", "none");
+
+            })
+                .catch(function (err) {
+                    console.log(err);
+                });
+        });
+    }
+
     /*******************toggle sidebar************************/
     $("button#burger").click(function () {
-
+        //$('div.controls').css("display", "");
         $("ul.side-menu, div.controls").toggleClass("display_none");
         $("#sidebar").toggleClass("col-sm-1 col-sm-3");
         $(".content").toggleClass("col-sm-11 col-sm-9");
@@ -218,6 +204,21 @@ $(function () {
 
     });
 
+    /*$('input[type=radio][name=optionsRadios]').change(function () {
+        if (this.value === 'option1') {
+            $(".content").css("background", "#d2d6a8");
+        }
+        else if (this.value === 'option2') {
+            $(".content").css("background", "#eee");
+        }
+        else if (this.value === 'option3') {
+            $(".content").css("background", "#fff");
+        }
+        else if (this.value === 'option4') {
+            $(".content").css("background", "#aaa");
+        }
+    });*/
+
     /*******************changing font-family******************/
     $("select#font_family").change(function () {
         $(".content p").css("font-family", $(this).val());
@@ -240,3 +241,15 @@ $(function () {
 
 
 });
+
+var user2 = {
+    name: 2225552,
+    id: new Date(),
+    email: 'simple@gmail.com'
+}
+
+/*localStorage.setItem('User', JSON.stringify(user2));
+
+console.log('User', localStorage.getItem('User'));
+console.log('UserObj', JSON.parse(localStorage.getItem('User')));*/
+
